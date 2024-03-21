8f1a66a40a7e76c1d361f1e5d67222fb69ea1e26
Seranged <seranged@gmail.com>
2024-03-21 21:15:58 +0000

    revert

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -7,7 +7,7 @@ import { serveStatic } from 'frog/serve-static'
 import { devtools } from 'frog/dev'
 
 const app = new Frog({
-  basePath: '/api/:dynamicUrl',
+  basePath: '/api/',
   hub: pinata(),
   browserLocation: '/',
 })
