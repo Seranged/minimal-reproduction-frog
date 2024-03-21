25f85b34693ebe0e3ab38ca14e62d7484244f831
Seranged <seranged@gmail.com>
2024-03-21 23:54:44 +0000

    revert

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -1,12 +1,10 @@
 /** @jsxImportSource frog/jsx */
 
-import { Frog } from 'frog'
+import { Button, Frog, TextInput } from 'frog'
 import { pinata } from 'frog/hubs'
 import { handle } from 'frog/next'
 import { serveStatic } from 'frog/serve-static'
 import { devtools } from 'frog/dev'
-import { app as transactionFrame } from '../../frames/transaction'
-import { app as rootFrame } from '../../frames/root'
 
 const app = new Frog({
   basePath: '/api',
@@ -14,10 +12,77 @@ const app = new Frog({
   browserLocation: '/',
 })
 
-app.route('/transaction', transactionFrame)
-app.route('/:dynamicUrl', rootFrame)
+app.transaction('/transaction', async (c) => {
+  const targetAddress = c.address
+  const shortId = [54]
+  const amountUSD = 5
+
+  const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
+  if (!response.ok) {
+    throw new Error('Failed to fetch exchange rate')
+  }
+  const data = await response.json()
+  const usdToEthRate = data.ethereum.usd
+  const amountETH = amountUSD / usdToEthRate
+  const chainsBN = shortId.reduce((p, c) => (p << BigInt(8)) + BigInt(c), BigInt(0))
+  const amountWei: bigint = BigInt(Math.floor(amountETH * 1e18))
+
+  return c.contract({
+    abi: depositABI,
+    chainId: 'eip155:10',
+    functionName: 'deposit',
+    value: amountWei,
+    to: '0x9e22ebec84c7e4c4bd6d4ae7ff6f4d436d6d8390',
+    args: [chainsBN, targetAddress as `0x${string}`],
+  })
+})
+
+app.frame('/:dynamicUrl', (c) => {
+  const dynamicUrl = c.req.param('dynamicUrl')
+  return c.res({
+    image: (
+      <div
+        style={{
+          height: '100vh',
+          width: '100vw',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#000000',
+        }}
+      >
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'white', fontSize: 60 }}>
+          Test Minimal Dynamic Url ID: {dynamicUrl}
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button.Transaction target="/transaction">10</Button.Transaction>,
+    ],
+  })
+})
 
 devtools(app, { serveStatic })
 export const GET = handle(app)
 export const POST = handle(app)
 
+const depositABI = [
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'chains',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+    ],
+    name: 'deposit',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+] as const
