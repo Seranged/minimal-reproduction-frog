5e92366aeb783d34b0e60f91c208f18c35eb69c0
Seranged <seranged@gmail.com>
2024-03-26 18:47:16 +0000

    issue on prop drilling renders minimal

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -1,48 +1,20 @@
 /** @jsxImportSource frog/jsx */
-
-import { Button, Frog, TextInput } from 'frog'
-import { pinata } from 'frog/hubs'
+import { Button, Frog } from 'frog'
 import { handle } from 'frog/next'
-import { serveStatic } from 'frog/serve-static'
 import { devtools } from 'frog/dev'
+import { serveStatic } from 'frog/serve-static'
 
 const app = new Frog({
   basePath: '/api',
-  hub: pinata(),
   browserLocation: '/',
 })
 
-app.transaction('/transaction', async (c) => {
-  const targetAddress = c.address
-  const shortId = [54]
-  const amountUSD = 5
-
-  const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
-  if (!response.ok) {
-    throw new Error('Failed to fetch exchange rate')
-  }
-  const data = await response.json()
-  const usdToEthRate = data.ethereum.usd
-  const amountETH = amountUSD / usdToEthRate
-  const chainsBN = shortId.reduce((p, c) => (p << BigInt(8)) + BigInt(c), BigInt(0))
-  const amountWei: bigint = BigInt(Math.floor(amountETH * 1e18))
-
-  return c.contract({
-    abi: depositABI,
-    chainId: 'eip155:10',
-    functionName: 'deposit',
-    value: amountWei,
-    to: '0x9e22ebec84c7e4c4bd6d4ae7ff6f4d436d6d8390',
-    args: [chainsBN, targetAddress as `0x${string}`],
-  })
-})
-
-app.frame('/:dynamicUrl', (c) => {
-  const dynamicUrl = c.req.param('dynamicUrl')
+app.frame('/', async (c) => {
   return c.res({
     image: (
       <div
         style={{
+          fontFamily: 'Inter',
           height: '100vh',
           width: '100vw',
           display: 'flex',
@@ -51,14 +23,55 @@ app.frame('/:dynamicUrl', (c) => {
           backgroundColor: '#000000',
         }}
       >
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'white', fontSize: 60 }}>
-          Test Minimal Dynamic Url ID: {dynamicUrl}
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <div style={{ color: 'white', fontSize: 60 }}>Test Frame 1 </div>
+          <div style={{ color: 'white', fontSize: 40 }}>
+            Test Frame 1 
+          </div>
         </div>
       </div>
     ),
     intents: [
-      <Button.Transaction target="/transaction">10</Button.Transaction>,
-    ],
+      <Button action={`/goToFrame2/testPropButton1`} value='10'>
+        Optimism
+      </Button>,
+      <Button action={`/goToFrame2/testPropButton2`} value='8453'>
+        Base
+      </Button>,
+    ]
+  })
+})
+
+app.frame('/goToFrame2/:testProp', (c) => { 
+  const { buttonValue } = c
+  const { testProp } = c.req.param()
+
+  console.log('buttonValue', buttonValue)
+  console.log('testProp', testProp)
+
+  return c.res({
+    image: (
+      <div
+        style={{
+          fontFamily: 'Inter',
+          height: '100vh',
+          width: '100vw',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#000000',
+        }}
+    >       
+     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+       <div style={{ color: 'white', fontSize: 60 }}>Test Frame 2</div>
+       <div style={{ color: 'white', fontSize: 40 }}>
+         Test Frame 2 (Works)
+         {/* Test Frame 2 - Button Value {buttonValue} (Doesn't work) */}
+         {/* Test Frame 2 - Prop Value {testProp} (Doesn't work) */}
+          </div>
+       </div>
+      </div>
+    )
   })
 })
 
@@ -66,23 +79,6 @@ devtools(app, { serveStatic })
 export const GET = handle(app)
 export const POST = handle(app)
 
-const depositABI = [
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'chains',
-        type: 'uint256',
-      },
-      {
-        internalType: 'address',
-        name: 'to',
-        type: 'address',
-      },
-    ],
-    name: 'deposit',
-    outputs: [],
-    stateMutability: 'payable',
-    type: 'function',
-  },
-] as const
+
+
+
